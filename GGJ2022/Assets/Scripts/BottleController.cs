14d2a9dcd5957772751afe94c6f08226fcddea62
Sara Uvalic <sara.uvalic@hotmail.com>
2022-01-29 18:33:22 +0100

    Added spinning to bottle

--- GGJ2022/Assets/Scripts/BottleController.cs
+++ GGJ2022/Assets/Scripts/BottleController.cs
@@ -2,96 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class BottleController : MonoBehaviour
 {
-    public int MaxPlayers = 6;
-    public List<string> CurrentPlayers;
+    public Rigidbody rigidbody;
+    public float forceStrengthMin;
+    public float forceStrengthMax;
+    public float velocityThreshold;
 
-    public string ChosenPlayer;
+    public Transform forcePos;
 
-    public List<Player> PlayerList;
-    public Text ChosenPlayerText;
-
-    public UnityEvent OnSpin;
-    public UnityEvent OnPlayerChoosen;
-
-    private InputField NewPlayer;
-
-    public int totalWeight;
-    public int weightAdded;
-    public List<int> weights;
-    
-    
+    public UnityEvent OnBottleSpin;
+    public UnityEvent OnBottleStop;
 
     // Start is called before the first frame update
     void Start()
     {
-        weights = new List<int>();
-        NewPlayer = FindObjectOfType<InputField>();
-        foreach (var item in PlayerList)
-	    {
-            item.gameObject.SetActive(false);
-	    }
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rigidbody.angularVelocity.z <= velocityThreshold)
+	    {
+            rigidbody.angularVelocity= Vector3.zero;
+            OnBottleStop.Invoke();
+	    }
     }
-
-    public void AddPlayer()
-	{
-        var name = NewPlayer.text;
-        var index = CurrentPlayers.Count -1;
-        if (CurrentPlayers.Count < MaxPlayers)
-	    {
-            CurrentPlayers.Add(name);
-            index = CurrentPlayers.Count-1;
-            PlayerList[index].name = name;
-            PlayerList[index].text.text = name;
-            PlayerList[index].gameObject.SetActive(true);
-	    }
-        if (CurrentPlayers.Count >= MaxPlayers)
-	    {
-            NewPlayer.gameObject.SetActive(false);
-            AllPlayersAdded();
-	    }
-	}
-
-    public void AllPlayersAdded()
-	{
-        weights.Clear();
-
-        for (int i = 0; i < CurrentPlayers.Count; i++)
-		{
-            weights.Add(weightAdded * 10);
-            totalWeight += weightAdded * 10;
-		}
-	}
 
     public void SpinBottle()
 	{
-        ChosenPlayer = ChoosePlayer();
-	}
-
-    private string ChoosePlayer()
-	{
-        var random = Random.Range(1, totalWeight + 1);
-        var extra = 0;
-
-        for (int i = 0; i < weights.Count; i++)
-		{
-            if(random <= weights[i] + extra)
-            {
-                weights[i] -= weightAdded;
-                totalWeight -= weightAdded;
-                return CurrentPlayers[i];
-            }
-            extra += weights[i];
-		}
-        return "";
+        var forceStrength = Random.Range(forceStrengthMin, forceStrengthMax);
+        rigidbody.AddForceAtPosition(forcePos.right * - forceStrength, forcePos.position, ForceMode.Impulse);
+        OnBottleSpin.Invoke();
 	}
 }
